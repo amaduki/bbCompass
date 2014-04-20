24b5be200867a4de2b3fe25cfaed6bc6fc0cec8f
Osamu Tsukiyama <o.tsukiyama@gmail.com>
2014-04-20 18:19:19 +0900

    マップの保管場所変更

--- include/bb_webfunc.js
+++ include/bb_webfunc.js
@@ -162,7 +162,7 @@ function chg_map() {
     var layer = eval($("#map option:selected").attr("data-layer"));
     var scale = eval($("#stage").children("[value='"+stage+"']").attr("data-scale"));
     var salt  = "?" + new Date().getTime();
-    bbobj.setbg("./map/"+file+".jpg" + salt, scale[0], scale[1],
+    bbobj.setbg("./map/"+stage+"/"+file+".jpg" + salt, scale[0], scale[1],
                 function(){
                     $("#"+DivName).width($("#"+CanvasName).outerWidth() + scrollBarWidth)
                                   .height($("#"+CanvasName).outerHeight() + scrollBarHeight);
